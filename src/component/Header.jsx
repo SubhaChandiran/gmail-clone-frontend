04896d317a6521e826afc17b22e1f8320ef2ee32
SubhaChandiran <jerrysubash@gmail.com>
2024-01-05 00:39:48 +0530

    settingOutlined + AppsOutlined + AccountCircleOutlined

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,22 +1,64 @@
 import React from 'react';
-import { AppBar, Toolbar, styled } from '@mui/material';
-import { Menu as MenuIcon } from '@mui/icons-material';
+import { AppBar, Toolbar, styled, InputBase, Box } from '@mui/material';
+import { Menu as MenuIcon, Search,Tune, HelpOutlineOutlined, SettingsOutlined, AppsOutlined, AccountCircleOutlined } from '@mui/icons-material';
 import { gmailLogo } from '../constants/constant';
 
-const styledAppBar = styled(AppBar)({
+const StyledAppBar = styled(AppBar)({
     background: '#f5f5f5',
     boxShadow: 'none'
+})
+
+const SearchWrapper = styled(Box)({
+    background: '#EAF1FB',
+    marginLeft: 80,
+    borderRadius: 8,
+    minWidth: 690,
+    maxWidth: 720,
+    height: 48,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '0 20px',
+    '& > div': {
+        width: '100%',
+        padding: '0 10px'
+    }
+});
+
+const OptionsWrapper = styled(Box)({
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'end',
+    '& > svg': {
+        marginLeft: 20
+    }
 })
 
 function Header() {
   return (
       <div>
-          <styledAppBar className='styledAppBar' position='static'>
+          <StyledAppBar className='styledAppBar' position='static'>
               <Toolbar>
                   <MenuIcon color='action' />
-                  <img src={gmailLogo} alt='logo' />
+                  <img src={gmailLogo} alt='logo' style={{ width: 110, marginLeft: 15 }} />
+                  <SearchWrapper>
+                      <Search color='action' />
+                      <InputBase
+                          placeholder='Search mail'
+                      />
+                      <Tune color='action' />
+                  </SearchWrapper>
+                  <OptionsWrapper>
+                      <HelpOutlineOutlined
+                          color='action' />
+                      <SettingsOutlined
+                          color='action' />
+                      <AppsOutlined
+                          color='action' />
+                      <AccountCircleOutlined color='action'/>
+                  </OptionsWrapper>
               </Toolbar>
-          </styledAppBar>
+          </StyledAppBar>
     </div>
   )
 }
--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Drawer, styled } from '@mui/material';
 
 function SiderBar() {
   return (
