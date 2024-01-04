19ed60664b455c1cb4191a9db9095c0bace14aae
SubhaChandiran <jerrysubash@gmail.com>
2024-01-05 00:51:39 +0530

    SideBar using Drawer

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -3,7 +3,28 @@ import { Drawer, styled } from '@mui/material';
 
 function SiderBar() {
   return (
-    <div>Hello from Sildbar</div>
+    <>
+      <Drawer
+        anchor='left'
+        open={true}
+        hideBackdrop={true}
+        ModalProps={{
+          keepMounted: true
+        }}
+        variant='parsistent'
+        sx={{
+          '& .MuiDrawer-paper': {
+            marginTop: '64px',
+            width: 250,
+            background: '#F5F5F5',
+            borderRight: 'none',
+            height: 'calc(100vh - 64px)'
+          }
+        }}
+      >
+        Helloooo
+      </Drawer>
+    </>
   )
 }
 
