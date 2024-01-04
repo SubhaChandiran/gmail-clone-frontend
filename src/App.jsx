6a21c448868ed1b0c8c6ee0825edbe8c63b1919c
SubhaChandiran <jerrysubash@gmail.com>
2024-01-04 22:42:21 +0530

    main + header + Sidebar + mui instal

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Main from '../src/pages/Main';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        Hello
+        <Main />
       </div>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,16 @@
+import Header from '../component/Header'
+import SiderBar from '../component/SiderBar';
+
+const Main = () => {
+    return (
+        <>
+            <div>
+                <Header />
+                <SiderBar />
+                <div>Display mails</div>
+            </div>
+        </>
+    )
+}
+
+export default Main;
