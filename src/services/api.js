6e619f0ed49af7f6e9a72d0d98e9e2bae014cf8f
SubhaChandiran <jerrysubash@gmail.com>
2024-01-18 00:09:10 +0530

    add backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // backend URL
-const API_URL = "http://localhost:8000";
+const API_URL = process.env.BACKEND_URL;
 
 const API_GMAIL = async (urlObject, payload, type) => {
   return await axios({
